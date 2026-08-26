55d08e13ccea62e775fc9d25214186030b8fe0d4
Sebastian <ssarmiento@cliandina.com>
2026-08-26 14:28:44 -0500

    Calculo de propina

--- app/src/main/java/com/clinet/learningasjc/MainActivity.kt
+++ app/src/main/java/com/clinet/learningasjc/MainActivity.kt
@@ -12,10 +12,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -24,11 +26,14 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -53,130 +58,117 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearningASJCTheme {
-                LemonadeApp()
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun LemonadeApp() {
-    var currentStep by remember() { mutableStateOf(1) }
-    var squeezeCount by remember() { mutableStateOf(0) }
-
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = "Lemonade",
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color(color = 0xFFF5E568)
-                )
-            )
-        }
-    ) { innerPadding ->
-        Surface(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-        ) {
-            when(currentStep){
-                1 -> {
-                    LemonTextWithImage(
-                        onImageClick = {
-                            currentStep = 2
-                            squeezeCount = (2..4).random()
-                        },
-                        text = R.string.lemon_tree,
-                        image = R.drawable.lemon_tree,
-                        contentDescription = R.string.lemon_tree_content_description
-                    )
-                }
-                2 -> {
-                    LemonTextWithImage(
-                        onImageClick = {
-                            squeezeCount--
-                            if (squeezeCount == 0) {
-                                currentStep = 3
-                            }
-                        },
-                        text = R.string.lemon_squeeze,
-                        image = R.drawable.lemon_squeeze,
-                        contentDescription = R.string.lemon_content_description
-                    )
-                }
-                3 -> {
-                    LemonTextWithImage(
-                        onImageClick = {
-                            currentStep = 4
-                        },
-                        text = R.string.lemon_drink,
-                        image = R.drawable.lemon_drink,
-                        contentDescription = R.string.lemonade_content_description
-                    )
-                }
-                4 -> {
-                    LemonTextWithImage(
-                        onImageClick = {
-                            currentStep = 1
-                        },
-                        text = R.string.lemon_restart,
-                        image = R.drawable.lemon_restart,
-                        contentDescription = R.string.empty_glass_content_description
-                    )
-                }
+                BillApp()
             }
         }
     }
 }
 
 @Composable
-fun LemonTextWithImage(
-    onImageClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    @StringRes text: Int,
-    @DrawableRes image: Int,
-    @StringRes contentDescription: Int
+fun BillApp() {
+    var mount by remember { mutableIntStateOf(0) }
+    var tip by remember { mutableIntStateOf(0) }
 
-    ) {
+    var roundedUp by remember { mutableStateOf(false) }
+
+    val totalTip = if (roundedUp) {
+        kotlin.math.ceil(mount * tip / 100.0)
+    } else {
+        (mount * tip / 100.0)
+    }
+
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                25.dp
+            ),
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(
-            onClick = onImageClick,
-            shape = RoundedCornerShape(dimensionResource(id = R.dimen.button_corner_radius)),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(color = 0xFFCBEBD4)
-            )
+        Text(text = stringResource(R.string.app_name))
+        TextFieldComponent(
+            value = mount.toString(),
+            onValueChange = { mount = it.toIntOrNull() ?: 0 },
+            label = { Text(stringResource(R.string.bill_amount)) },
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            leadingIcon = {
+                Image(
+                    painter = painterResource(R.drawable.money),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextFieldComponent(
+            // icono
+            value = tip.toString(),
+            onValueChange = { tip = it.toIntOrNull() ?: 0 },
+            label = { Text(stringResource(R.string.how_was_the_service)) },
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            leadingIcon = {
+                Image(
+                    painter = painterResource(R.drawable.percent),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Image(
-                painter = painterResource(id = image),
-                contentDescription = stringResource(id = contentDescription),
+            Text(
+                text = stringResource(R.string.round_up_tip),
+            )
+            Switch(
+                checked = roundedUp,
+                onCheckedChange = {
+                    roundedUp = it
+                },
                 modifier = Modifier
-                    .width(dimensionResource(id = R.dimen.button_image_with))
-                    .height(dimensionResource(id = R.dimen.button_image_height))
-                    .padding(dimensionResource(id = R.dimen.button_interior_padding))
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End)
             )
         }
-        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_vertical)))
         Text(
-            text = stringResource(id = text),
-            fontSize = 18.sp,
-            textAlign = TextAlign.Center
+            text = stringResource(R.string.tip_amount, totalTip),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
         )
     }
+}
+
+@Composable
+fun TextFieldComponent(
+    modifier: Modifier = Modifier,
+    value: String = "",
+    onValueChange: (String) -> Unit = {},
+    label: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = label,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        leadingIcon = leadingIcon,
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LearningASJCTheme {
-        LemonadeApp()
+        BillApp()
     }
 }
